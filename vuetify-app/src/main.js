b3262216b27a2ae6caaef64b0f5437097c9f9358
christ <christmokonda@gmail.com>
2022-03-25 10:34:53 +0100

    Vuetify added correctly

--- vuetify-app/src/main.js
+++ vuetify-app/src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
+import Vue  from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
 
-createApp(App).mount('#app')
+// eslint-disable-next-line no-undef
+new Vue({
+    // eslint-disable-next-line no-undef
+    vuetify,
+    render: h => h(App)
+}).$mount('#app');
